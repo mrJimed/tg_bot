277a23d176af1b4ea9da8b171862dde656702479
Danil <danil0316@mail.ru>
2024-12-07 19:04:10 +0500

    add start message

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ from config.app_config import TG_BOT_TOKEN
 bot = telebot.TeleBot(TG_BOT_TOKEN)
 
 
+@bot.message_handler(commands=['start'])
+def start_message(message: Message):
+    bot.send_message(message.from_user.id,
+                     'Здравствуйте! Данный бот предназначен для генерации краткой текстовой аннотации аудио и текстового контента в Telegram. Чтобы получить аннотацию отправьте мне текстовое или аудио сообщение.')
+
+
 @bot.message_handler(content_types=['voice'])
 def annotation_audio(message: Message) -> None:
     file_id = message.voice.file_id
